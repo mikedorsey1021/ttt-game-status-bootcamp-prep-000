9aa1463ed34528f8c6c1ba330be3fcfcfd0b68ec
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-23 06:31:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,7 +54,7 @@ def draw? (board)
 end
 
 def over? (board)
-  if won?(board) == true || draw?(board) == true || full?(board) == false
+  if won?(board) == true || draw?(board) == true || full?(board) == true
     return true
   else
     return false
