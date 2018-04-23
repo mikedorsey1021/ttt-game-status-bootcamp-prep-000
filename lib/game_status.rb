4f354067fe4c7da450490ea6cac75041d3d16cf2
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-23 05:14:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,19 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
+
+def won (board)
+  counter_x = 0
+  counter_o = 0
+  WIN_COMBINATIONS.each do |combinations|
+    combinations.each do |index|
+      if board[index] == "X"
+        counter_x += 1
+      elsif board[index] == "O"
+        counter_o += 1
+      else
+        counter = 0
+      end
+
+  end
+end
